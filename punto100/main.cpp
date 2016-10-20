fb47f2845a9b2d9226e096ae5a5f09e8872c5035
Daniela Mazza <danielamazza@gmail.com>
2016-10-20 14:45:40 +0200

    Created by Coding Ground Client

---
+++ punto100/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+for (int i=0;i<10000;i++){
+    if (i%100 == 0){
+        cout << "." << i << endl;
+    }
+}    
+ 
+   return 0;
+}
+
