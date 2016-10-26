bb8c34bf9fdfa825a9466a28555f719ec9781f7e
Daniela Mazza <danielamazza@gmail.com>
2016-10-26 12:24:13 +0200

    Update main.cpp

--- NewOperator/main.cpp
+++ NewOperator/main.cpp
@@ -2,10 +2,37 @@
 
 using namespace std;
 
+class Animal {
+private:
+    string name;
+public:
+    Animal(){
+        cout << "Animal created" << endl;
+    }
+    
+    Animal(const Animal &other):
+        name(other.name){
+        cout << "Animal created by copy" << endl;
+    } 
+    
+    ~Animal(){
+        cout << "Destructor called" << endl;
+    }
+    void setName(string name){
+        this->name = name;
+    }
+    void speak(){
+        cout << "My name is: " << name << endl;
+    }
+};
+
+
 int main()
 {
-   cout << "Hello World" << endl; 
-   
+   Animal *pCat1 = new Animal();
+   pCat1->setName("Milo");
+   pCat1->speak();
+   delete pCat1;
    return 0;
 }
 
